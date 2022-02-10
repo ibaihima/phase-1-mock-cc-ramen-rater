c4a09d12f2b42a4d96b0b77976f58f7e2b5f9ca8
Ibaihima <ibaihima@hotmail.com>
2022-02-10 11:15:59 -0500

    commit tree 55fdebe8d6655509bb01a4a851130c5695537c0a

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+// See all ramen images in the div with the id of ramen-menu. 
+// When the page loads, request the data from the server to get all the ramen objects.
+// Then, display the image for each of the ramen using an img tag inside the #ramen-menu div.
+
 // write your code here
+const init = () => {
+        document.getElementById('ramen-menu').appendChild(addImg)
+        
+    const connect = () => {
+        fetch('http://localhost:3000/ramens')
+        .then(responce => responce.json())
+        .then(data => console.log(data))
+    }
+}    
+const addImg = () => {
+        const img = document.createElement('img')
+        const eachImg = (array) =>{
+        array.forEach(element => {
+            element.image
+
+         });
+         console.log(eachImg)
+        }
+    }    
